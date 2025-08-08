e4798ce9164976056bbf77449d084e482095f47d
tusharchindaliya <tusharchindaliya51@gmail.com>
2025-08-08 19:26:37 +0530

    array 3

--- array1.c
+++ array1.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 
-int main()
+int main() 
 {
-    int m[10];
-    int s=0;
-    float avg=0;
-    int i;
-    printf("enter 10 elements");
-    for(i=0 ; i<10 ; i++)
+    int m[10] , se ;
+    int mx=0;
+    int mn=0;
+    printf("enter 10 values");
+    
+    for(int i=0 ; i<10 ; i++)
     {
-        scanf("%d", & m[i]);
+        scanf("%d", &m[i]);
         
     }
-    s= m[0]+ m[1] + m[2] + m[3] + m[4] + m[5] + m[6] + m[7] + m[8] + m[9];
-    printf("sum=%d", s);
     
-    avg=s/10.0;
-    printf("\n avg=%.2f \n", avg);
+    mx=m[0];
     
+    for(int i=0 ; i<10 ; i++)
+    {
+        if(m[i]>mx)
+        mx=m[i];
+        
+    }
+    
+    printf("%d",mx);
+    
+    mn=m[0];
+    
+    for(int i=0 ; i<10 ; i++)
+    {
+        if(m[i]<mn)
+        mn=m[i];
+        
+    }
+    printf("\n%d",mn);
     return 0;
-    
 }
 
-    
